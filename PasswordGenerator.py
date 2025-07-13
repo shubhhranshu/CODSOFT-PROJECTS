45082da64f3ca751cd25064fe31692b7d5a42f34
Shubhranshu Tiwari <shubhranshutiwari9p@gmail.com>
2025-07-13 16:37:36 +0530

    Add files via upload

---
+++ PasswordGenerator.py
@@ -0,0 +1,11 @@
+import random
+
+chars = "@!#$%&*/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+length = int(input("Enter password length: "))
+
+password = ""
+for i in range(length):
+    password += random.choice(chars)
+
+print("Your password is:", password)
